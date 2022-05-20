aebe8c5f2a34230c2f6749b089791095a29310be
itsbriantime <superbrian69@gmail.com>
2022-05-19 22:24:36 -0600

    REMOVE STATIC FROM MAGIC TEXT made magic text a non-static class

--- src/main/java/terminalRpg/magictext/MagicText.java
+++ src/main/java/terminalRpg/magictext/MagicText.java
@@ -13,59 +13,63 @@
 package terminalrpg.magictext;
 
 public class MagicText {
-    // Reset
-    public static final String RESET = "\u001b[0m";
     
+    // Reset
+    public final String RESET = "\u001b[0m";
 
     // Color text
-    public static final String BLACK = "black";  
-    public static final String RED = "red";     
-    public static final String GREEN = "green";   
-    public static final String YELLOW = "yellow";  
-    public static final String BLUE = "blue";    
-    public static final String PURPLE = "purple";  
-    public static final String CYAN = "cyan";    
-    public static final String WHITE = "white";
-    public static final String RAINBOW = "rainbow";
+    public final String BLACK = "black";  
+    public final String RED = "red";     
+    public final String GREEN = "green";   
+    public final String YELLOW = "yellow";  
+    public final String BLUE = "blue";    
+    public final String PURPLE = "purple";  
+    public final String CYAN = "cyan";    
+    public final String WHITE = "white";
+    public final String RAINBOW = "rainbow";
     
     // formatting constants
-    public static final int REGULAR = 0;      
-    public static final int BRIGHT = 1;     
-    public static final int BACKGROUND = 2;  
-    public static final int BACKGROUNDBRIGHT = 3;    
+    public final int REGULAR = 0;      
+    public final int BRIGHT = 1;     
+    public final int BACKGROUND = 2;  
+    public final int BACKGROUNDBRIGHT = 3;    
     
     // Styles
-    public static final String BOLD = "\u001b[1m";
-    public static final String UNDERLINED = "\u001b[4m";
-    public static final String REVERSED = "\u001b[7m";
+    public final String BOLD = "\u001b[1m";
+    public final String UNDERLINED = "\u001b[4m";
+    public final String REVERSED = "\u001b[7m";
 
     // formatting arrays
-    private static final String[] BlACK_TEXT = {"\u001b[30m", "\u001b[30;1m", 
+    private String[] BlACK_TEXT = {"\u001b[30m", "\u001b[30;1m", 
         "\u001b[40m", "\u001b[40;1m"};
 
-    private static final String[] RED_TEXT = {"\u001b[31m", "\u001b[31;1m", 
+    private String[] RED_TEXT = {"\u001b[31m", "\u001b[31;1m", 
         "\u001b[41m", "\u001b[41;1m"};
 
-    private static final String[] GREEN_TEXT = {"\u001b[32m", "\u001b[32;1m", 
+    private String[] GREEN_TEXT = {"\u001b[32m", "\u001b[32;1m", 
         "\u001b[42m", "\u001b[42;1m"};
 
-    private static final String[] YELLOW_TEXT = {"\u001b[33m", "\u001b[33;1m", 
+    private String[] YELLOW_TEXT = {"\u001b[33m", "\u001b[33;1m", 
         "\u001b[43m", "\u001b[43;1m"};
 
-    private static final String[] BLUE_TEXT = {"\u001b[34m", "\u001b[34;1m", 
+    private String[] BLUE_TEXT = {"\u001b[34m", "\u001b[34;1m", 
         "\u001b[44m", "\u001b[44;1m"};
     
-    private static final String[] PURPLE_TEXT = {"\u001b[35m", "\u001b[35;1m", 
+    private String[] PURPLE_TEXT = {"\u001b[35m", "\u001b[35;1m", 
         "\u001b[45m", "\u001b[45;1m"};
 
-    private static final String[] CYAN_TEXT = {"\u001b[36m", "\u001b[36;1m", 
+    private String[] CYAN_TEXT = {"\u001b[36m", "\u001b[36;1m", 
         "\u001b[46m", "\u001b[46;1m"};
 
-    private static final String[] WHITE_TEXT = {"\u001b[37m", "\u001b[37;1m", 
+    private String[] WHITE_TEXT = {"\u001b[37m", "\u001b[37;1m", 
         "\u001b[47m", "\u001b[47;1m"};   
     
-    private static final String[] RAINBOW_TEXT = {"\u001b[31;1m", "\u001b[33;1m", 
+    private String[] RAINBOW_TEXT = {"\u001b[31;1m", "\u001b[33;1m", 
         "\u001b[34;1m", "\u001b[36;1m", "\u001b[32;1m", "\u001b[35;1m"};
+
+    public MagicText() {
+
+    }
     
     /** 
      * Draws colored text to the terminal based on the choices given. It can be
@@ -79,7 +83,7 @@ public class MagicText {
      * @param underlined adds underline to text
      * @param reversed reverses colors with background
      */
-    public static boolean drawColoredText(String text, String color, int format, 
+    public boolean drawColoredText(String text, String color, int format, 
         boolean bold, boolean underlined, boolean reversed) {
 
         String coloredCode = colorPicker(color, format, text);
@@ -98,7 +102,7 @@ public class MagicText {
      *      blue, purple, cyan or white.
      * @param format int that represents the text formatting desired
      */
-    public static String getColoredText(String text, String color, int format,
+    public String getColoredText(String text, String color, int format,
         boolean bold, boolean underlined, boolean reversed) {
 
         String coloredCode = stylePicker(bold, underlined, reversed);
@@ -111,7 +115,7 @@ public class MagicText {
      * Moves cursor up and delete the line.
      * @param lines lines to move
      */
-    public static boolean resetLines(int lines) {
+    public boolean resetLines(int lines) {
         // runs an extra time to clear current line
         for (int l = 0; l <= lines; l++) {
             cursorUp(1);
@@ -124,7 +128,7 @@ public class MagicText {
      * Moves cursor up.
      * @param numLines lines to move
      */
-    public static boolean cursorUp(int numLines) {
+    public boolean cursorUp(int numLines) {
         System.out.print("\u001b[" + numLines + "A");
         return true;
     }
@@ -133,7 +137,7 @@ public class MagicText {
      * Moves cursor down.
      * @param numLines lines to move
      */
-    public static boolean cursorDown(int numLines) {
+    public boolean cursorDown(int numLines) {
         System.out.print("\u001b[" + numLines + "B");
         return true;
     }
@@ -142,7 +146,7 @@ public class MagicText {
      * Moves cursor right.
      * @param numSpaces spaces to move
      */
-    public static boolean cursorRight(int numSpaces) {
+    public boolean cursorRight(int numSpaces) {
         System.out.print("\u001b[" + numSpaces + "C");
         return true;
     }
@@ -151,7 +155,7 @@ public class MagicText {
      * Moves cursor left.
      * @param numSpaces spaces to move
      */
-    public static boolean cursorLeft(int numSpaces) {
+    public boolean cursorLeft(int numSpaces) {
         System.out.print("\u001b[" + numSpaces + "D");
         return true;
     }
@@ -160,7 +164,7 @@ public class MagicText {
      * Moves curosr to beginning of the next line.
      * @param numLines lines to move down
      */
-    public static boolean nextLine(int numLines) {
+    public boolean nextLine(int numLines) {
         System.out.print("\u001b[" + numLines + "E");
         return true;
     }
@@ -169,7 +173,7 @@ public class MagicText {
      * Moves cursor to beginning of the pevious line.
      * @param numLines lines to move up
      */
-    public static boolean previousLine(int numLines) {
+    public boolean previousLine(int numLines) {
         System.out.print("\u001b[" + numLines + "F");
         return true;
     }
@@ -178,7 +182,7 @@ public class MagicText {
      * Moves cursor to column.
      * @param column value of desired column
      */
-    public static boolean setCursorColumn(int column) {
+    public boolean setCursorColumn(int column) {
         System.out.print("\u001b[" + column + "G");
         return true;
     }
@@ -188,7 +192,7 @@ public class MagicText {
      * @param row number of left/right desired position
      * @param col number of up/down desired position
      */
-    public static boolean setCursorPosition(int row, int col) {
+    public boolean setCursorPosition(int row, int col) {
         System.out.print("\u001b[" + row + ";" + col + "H");
         return true;
     }
@@ -199,7 +203,7 @@ public class MagicText {
      *     screen (0), chooses clear from cursor to beginning of screen (1),
      *     chooses to clear entire screen (2)
      */
-    public static boolean clearScreen(int cursorLocation) {
+    public boolean clearScreen(int cursorLocation) {
         System.out.print("\u001b[" + cursorLocation + "J");
         System.out.flush();
         return true;
@@ -210,7 +214,7 @@ public class MagicText {
      * @param clearFrom Choose to clear from cursor to end of line (0), Choose
      *     to clear from cursor to start of line (1). Clear entire line (2)
      */
-    public static boolean clearLine(int clearFrom) {
+    public boolean clearLine(int clearFrom) {
         System.out.print("\u001b[" + clearFrom + "K");
         return true;
     }
@@ -218,7 +222,7 @@ public class MagicText {
     /** 
      * Saves the position the cursor is in. 
      */
-    public static boolean saveCursorPosition() {
+    public boolean saveCursorPosition() {
         System.out.print("\u001b[s");
         return true;
     }
@@ -226,12 +230,12 @@ public class MagicText {
     /** 
      * Loads the position the cursor was in when it was saved. 
      */
-    public static boolean loadCursorPosition() {
+    public boolean loadCursorPosition() {
         System.out.print("\u001b[u");
         return true;
     }
     
-    private static String colorPicker(String color, int format, String text) {
+    private String colorPicker(String color, int format, String text) {
         switch (color) {
             case BLACK:
                 return BlACK_TEXT[format] + text;
@@ -264,7 +268,7 @@ public class MagicText {
         }
     }
 
-    private static String rainbowColor(String text) {
+    private String rainbowColor(String text) {
         String newRainbowText = "";
         int rainbowIterator = 0;
         int rainbowMultiplier = 1;
@@ -285,7 +289,7 @@ public class MagicText {
         return newRainbowText;
     }   
 
-    private static String stylePicker(boolean bold, boolean underlined, boolean reversed) {
+    private String stylePicker(boolean bold, boolean underlined, boolean reversed) {
         String addedStyle = "";
         if (bold) {
             addedStyle += BOLD;
@@ -297,7 +301,6 @@ public class MagicText {
         if (reversed) {
             addedStyle += REVERSED;
         }
-
         return addedStyle;
     }
 }
